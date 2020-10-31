98a3fe114778b15512b5445ec14c2868738071ae
german@scuniversoft.com <german@scuniversoft.com>
2020-10-31 08:02:31 -0600

    login y registro terminado

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/widgets/boton_azul.dart';
+import 'package:flutter_chat/widgets/custom_input.dart';
+import 'package:flutter_chat/widgets/labels.dart';
+import 'package:flutter_chat/widgets/logo.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Logo(titulo: 'Registro'),
+                  _Form(),
+                  Labels(
+                    ruta: 'login',
+                    texto: 'Ingresar ahora!!',
+                    titulo: 'Ya tienes cuenta?',
+                  ),
+                  Text(
+                    'Terminos y condiciones de uso',
+                    style: TextStyle(fontWeight: FontWeight.w200),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  final nameCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.perm_identity,
+            placeholder: 'Nombre',
+            keyboardType: TextInputType.text,
+            textController: nameCtrl,
+          ),
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contraseña',
+            textController: passCtrl,
+            isPasword: true,
+          ),
+          //TODO: Crear boton
+          BotonAzul(
+            text: 'Ingresar ahora',
+            onPressed: () {
+              print(emailCtrl.text);
+              print(passCtrl.text);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
